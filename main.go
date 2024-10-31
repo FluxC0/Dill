e3dcb149b47b1a1be6ab8ba0f859464b821b54d6
Kasen Engel <kasen.engel@protonmail.com>
2024-10-31 07:05:15 -0400

    Further efforts to parse JSOn

--- main.go
+++ main.go
@@ -1,31 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
-  "os"
+	"os"
 )
+
 type Config struct {
-  Package_Managers  string[]  `json:"package-managers"`
-  
+	Package_Managers []string `json:"packagemanagers"`
+	Authenticator    string `json:"authenticator"`
+	Concurrency      bool   `json:concurrency`
 }
+
 func parse() {
 	isDangerous := flag.Bool("dangerous", false, "turns on danger mode if set, which bypasses all user checks. Tread lightly when using this flag.")
 	flag.Parse()
-  file, err := os.Open("~/.config/dill/config.json")
-  if err != nil {
-    fmt.Println("failed to open config. Exit.")
-    return
-  }
-  defer file.Close()
-	main_loop(*isDangerous)
+
+	file, err := os.Open("/home/kengel/.config/dill/config.json")
+	if err != nil {
+		fmt.Println("failed to open config. Exit.", err)
+		return
+	}
+	defer file.Close()
+
+	var config Config
+
+  decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		fmt.Println("failed to decode config. Exit.", err)
+		return
+	}
+	main_loop(*isDangerous, config)
 }
 
-func main_loop(isDangerous bool, config any) {
+func main_loop(isDangerous bool, config Config) {
 	fmt.Println("Welcome to Dill! The meta-package-manager.")
 	if isDangerous {
 		fmt.Println("Danger mode enabled! Here be dragons...")
 	}
+
 }
 
 func main() {
